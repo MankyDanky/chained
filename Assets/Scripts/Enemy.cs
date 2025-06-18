4478772c71497a6b160e4fcd42183778e789ad21
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-06-18 15:16:35 -0400

    boss healthbar more

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public abstract class Enemy : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public abstract class Enemy : MonoBehaviour
     protected float healthBarWidth;
     protected Canvas healthBarCanvas;
     protected RectTransform healthBarIndicatorFill;
+    protected Image healthBarFillImage;
+    protected Image healthBarIndicatorFillImage;
     protected float healthBarIndicatorWidth;
     protected float healthBarIndicatorTimer = 0f;
     protected bool isDead = false;
@@ -19,6 +22,7 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected GameObject hitEffect;
     [SerializeField] float deathDelay = 1.5f;
     protected NavMeshAgent agent;
+    [SerializeField] bool isBoss = false;
 
     public float maxHealth = 100f;
     public float health;
@@ -45,7 +49,7 @@ public abstract class Enemy : MonoBehaviour
         Destroy(this.gameObject, deathDelay);
 
     }
-    
+
     protected virtual void OnDestroy()
     {
         Instantiate(destroyEffect, transform.position, Quaternion.identity);
@@ -57,38 +61,72 @@ public abstract class Enemy : MonoBehaviour
         player = FirstPersonController.Instance;
         playerController = player.GetComponent<FirstPersonController>();
         animator = GetComponent<Animator>();
-        healthBarFill = transform.Find("HealthBar/Health")?.GetComponent<RectTransform>();
-        healthBarIndicatorFill = transform.Find("HealthBar/Indicator")?.GetComponent<RectTransform>();
-        healthBarCanvas = transform.Find("HealthBar")?.GetComponent<Canvas>();
-        healthBarWidth = healthBarFill.sizeDelta.x;
+        if (!isBoss)
+        {
+            healthBarFill = transform.Find("HealthBar/Health")?.GetComponent<RectTransform>();
+            healthBarIndicatorFill = transform.Find("HealthBar/Indicator")?.GetComponent<RectTransform>();
+            healthBarCanvas = transform.Find("HealthBar")?.GetComponent<Canvas>();
+            healthBarWidth = healthBarFill.sizeDelta.x;
+        }
+        else
+        {
+            GameObject bossHealthBar = GameObject.Find("Canvas").transform.Find("BossHealthBar")?.gameObject;
+            bossHealthBar.SetActive(true);
+            healthBarFill = bossHealthBar.transform.Find("Health")?.GetComponent<RectTransform>();
+            healthBarIndicatorFill = bossHealthBar.transform.Find("Indicator")?.GetComponent<RectTransform>();
+            healthBarFillImage = healthBarFill.GetComponent<Image>();
+            healthBarIndicatorFillImage = healthBarIndicatorFill.GetComponent<Image>();
+        }
         agent = GetComponent<NavMeshAgent>();
     }
 
     protected virtual void Update()
     {
         UpdateHealthBarVisibility();
+
     }
 
     protected void UpdateHealthBarVisibility()
     {
-        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        Vector2 mousePos = Input.mousePosition;
+        if (!isBoss)
+        {
 
-        float distanceToMouse = Vector2.Distance(screenPos, mousePos);
-        float visibilityThreshold = 100f;
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+            Vector2 mousePos = Input.mousePosition;
 
-        healthBarCanvas.enabled = distanceToMouse < visibilityThreshold && (transform.position - player.position).magnitude < 30f;
+            float distanceToMouse = Vector2.Distance(screenPos, mousePos);
+            float visibilityThreshold = 100f;
+            healthBarCanvas.enabled = distanceToMouse < visibilityThreshold && (transform.position - player.position).magnitude < 30f;
 
-        healthBarIndicatorTimer += Time.deltaTime;
-        healthBarIndicatorTimer = Mathf.Clamp01(healthBarIndicatorTimer);
-        healthBarIndicatorFill.sizeDelta = new Vector2(Mathf.Lerp(healthBarIndicatorWidth, healthBarFill.sizeDelta.x, healthBarIndicatorTimer), healthBarIndicatorFill.sizeDelta.y);
+
+            healthBarIndicatorTimer += Time.deltaTime;
+            healthBarIndicatorTimer = Mathf.Clamp01(healthBarIndicatorTimer);
+            healthBarIndicatorFill.sizeDelta = new Vector2(Mathf.Lerp(healthBarIndicatorWidth, healthBarFill.sizeDelta.x, healthBarIndicatorTimer), healthBarIndicatorFill.sizeDelta.y);
+        }
+        else
+        {
+            healthBarIndicatorTimer += Time.deltaTime;
+            healthBarIndicatorTimer = Mathf.Clamp01(healthBarIndicatorTimer);
+            healthBarIndicatorFillImage.fillAmount = Mathf.Lerp(healthBarIndicatorWidth, healthBarFillImage.fillAmount, healthBarIndicatorTimer);
+        }
     }
 
     protected void UpdateHealthBar()
     {
-        float healthPercent = Mathf.Clamp01(health / maxHealth);
-        healthBarFill.sizeDelta = new Vector2(healthBarWidth * healthPercent, healthBarFill.sizeDelta.y);
-        healthBarIndicatorTimer = 0.0f;
-        healthBarIndicatorWidth = healthBarIndicatorFill.sizeDelta.x;
+        if (!isBoss)
+        {
+            float healthPercent = Mathf.Clamp01(health / maxHealth);
+            healthBarFill.sizeDelta = new Vector2(healthBarWidth * healthPercent, healthBarFill.sizeDelta.y);
+            healthBarIndicatorTimer = 0.0f;
+            healthBarIndicatorWidth = healthBarIndicatorFill.sizeDelta.x;
+        }
+        else
+        {
+            float healthPercent = Mathf.Clamp01(health / maxHealth);
+            healthBarFillImage.fillAmount = healthPercent;
+            healthBarIndicatorTimer = 0.0f;
+            healthBarIndicatorWidth = healthBarIndicatorFillImage.fillAmount;
+        }
+        
     }
 }
